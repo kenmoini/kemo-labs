f14328550423e0fe8418f71f27ca4c84b60f8d48
Ken Moini <ken@kenmoini.com>
2026-04-18 12:35:57 -0400

    db init glue

--- databases/shared/init-scripts/echo-test.sh
+++ databases/shared/init-scripts/echo-test.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/with-contenv bash
 
-echo "Hello from the MariaDB init script!"
+echo "==============================================================="
+echo "Bootstrapping MariaDB container with needed databases..."
+echo "==============================================================="
+
+while [ $(/usr/local/bin/healthcheck.sh --connect --innodb_initialized) -ne 0 ]; do
+  echo "Waiting for MariaDB to initialize..."
+  sleep 5
+done
+echo "MariaDB is initialized. Creating databases..."
+
+# Create databases
+/docker-entrypoint-initdb.d/01-create-databases.sh
+
+# Go to sleep
 sleep infinity

8db86467fa6da8bfea0ac49590810ed4208a13c8
Ken Moini <ken@kenmoini.com>
2026-05-27 22:46:54 -0400

    bump for nexus

--- scripts/stop-caas-quadlets.sh
+++ scripts/stop-caas-quadlets.sh
@@ -9,11 +9,12 @@ systemctl stop caas-dozzle
 systemctl stop caas-auto-kuma
 systemctl stop caas-stepca
 systemctl stop caas-dockns
+systemctl stop caas-nexus
 systemctl stop caas-authentik-pod
 systemctl stop caas-uptime-kuma
 systemctl stop caas-dns-pod
 systemctl stop caas-shared-db-pod
-systemctl stop caas-pki
+systemctl stop caas-pki-nginx
 systemctl stop caas-traefik
 systemctl stop caas-podman-proxy
 systemctl stop caas-chrony

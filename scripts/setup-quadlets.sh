2d291d06032653f59ab6d5e80a2531b488222d77
Ken Moini <ken@kenmoini.com>
2026-05-28 21:59:45 -0400

    vault quadlet

--- scripts/setup-quadlets.sh
+++ scripts/setup-quadlets.sh
@@ -38,7 +38,7 @@ podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/security/identi
 podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/utilities/dockns/dockns.quadlets
 podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/security/acme/acme.quadlets
 podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/storage/dropbox/dropbox.quadlets
-# podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/security/vault/vault.quadlets
+podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/security/vault/vault.quadlets
 podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/storage/s3/s3.quadlets
 podman quadlet install --replace --reload-systemd ${GIT_SRC_DIR}/storage/container-registry/nexus.quadlets
 
